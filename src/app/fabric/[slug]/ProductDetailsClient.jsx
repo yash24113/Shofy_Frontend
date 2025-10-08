6b9ad0a9f25d45f5e800a31007881a345fa44241
Yash Khalas <yashkharva506@gmail.com>
2025-10-08 18:29:04 +0530

    fix

--- src/app/fabric/[slug]/ProductDetailsClient.jsx
+++ src/app/fabric/[slug]/ProductDetailsClient.jsx
@@ -3,58 +3,73 @@
 
 import React from 'react';
 
-import ProductDetailsArea   from '@/components/product-details/product-details-area';
+import ProductDetailsArea from '@/components/product-details/product-details-area';
 import ProductDetailsLoader from '@/components/loader/prd-details-loader';
-import ErrorMsg             from '@/components/common/error-msg';
+import ErrorMsg from '@/components/common/error-msg';
 
 import { useGetSingleNewProductQuery } from '@/redux/features/newProductApi';
 
 
 function mapBackendProductToFrontend(p) {
+  const mainImg = p.img || p.image || '';
+  const img1 = p.image1 || '';
+  const img2 = p.image2 || '';
+  const videoUrl = p.video || '';
+  const poster = p.videoThumbnail || '';
+
   const images = [
-    p.image  && { img: p.image,  type: 'image' },
-    p.image1 && { img: p.image1, type: 'image' },
-    p.image2 && { img: p.image2, type: 'image' },
+    mainImg && { type: 'image', img: mainImg },
+    img1 && { type: 'image', img: img1 },
+    img2 && { type: 'image', img: img2 },
   ].filter(Boolean);
 
-  if (p.video && p.videoThumbnail) {
-    images.push({ img: p.videoThumbnail, type: 'video' });
+  if (videoUrl || poster) {
+    images.push({ type: 'video', img: poster || mainImg || img1 || img2, video: videoUrl });
   }
 
   return {
-    _id        : p._id,
-    slug       : p.slug,
-    title      : p.name || p.title,
-    img        : p.image || '',
-    imageURLs  : images,
-    videoId    : p.video || '',
-    price      : p.salesPrice,
-     description: p.description || p.productdescription || '',
-    status     : p.status || 'in-stock',
-    sku        : p.sku,
+    _id: p._id,
+    slug: p.slug,
+    title: p.name || p.title,
+    img: mainImg,
+    image1: img1,
+    image2: img2,
+    video: videoUrl,
+    videoThumbnail: poster,
+    // expose raw fields needed for Details components
+    color: p.color || p.colors || [],
+    colors: p.colors || [],
+    motif: p.motif || p.motifsize || null,
+    motifId: (p.motif && p.motif._id) || p.motif || p.motifsize || null,
+    imageURLs: images,
+    videoId: videoUrl,
+    price: p.salesPrice,
+    description: p.description || p.productdescription || '',
+    status: p.status || 'in-stock',
+    sku: p.sku,
 
-    categoryId     : p.category?._id      || p.category      || '',
-    structureId    : p.substructure?._id  || p.substructure  || '',
-    contentId      : p.content?._id       || p.content       || '',
-    finishId       : p.subfinish?._id     || p.subfinish     || '',
-    designId       : p.design?._id        || p.design        || '',
-    motifsizeId    : p.motif?._id         || p.motif         || '',
-    suitableforId  : p.subsuitable?._id   || p.subsuitable   || '',
-    vendorId       : p.vendor?._id        || p.vendor        || '',
-    groupcodeId    : p.groupcode?._id     || p.groupcode     || '',
+    categoryId: p.category?._id || p.category || '',
+    structureId: p.substructure?._id || p.substructure || '',
+    contentId: p.content?._id || p.content || '',
+    finishId: p.subfinish?._id || p.subfinish || '',
+    designId: p.design?._id || p.design || '',
+    motifsizeId: p.motif?._id || p.motif || '',
+    suitableforId: p.subsuitable?._id || p.subsuitable || '',
+    vendorId: p.vendor?._id || p.vendor || '',
+    groupcodeId: p.groupcode?._id || p.groupcode || '',
 
-    gsm               : p.gsm,
-    oz                : p.oz,
-    productIdentifier : p.productIdentifier,
-    width             : p.cm
-                          ? `${p.cm} cm`
-                          : p.inch
-                          ? `${p.inch} inch`
-                          : 'N/A',
+    gsm: p.gsm,
+    oz: p.oz,
+    productIdentifier: p.productIdentifier,
+    width: p.cm
+      ? `${p.cm} cm`
+      : p.inch
+        ? `${p.inch} inch`
+        : 'N/A',
 
-    tags                  : p.tags || [],
-    offerDate             : p.offerDate || { endDate: null },
-    additionalInformation : p.additionalInformation || [],
+    tags: p.tags || [],
+    offerDate: p.offerDate || { endDate: null },
+    additionalInformation: p.additionalInformation || [],
   };
 }
 
@@ -65,8 +80,8 @@ export default function ProductDetailsClient({ slug }) {
     isError,
   } = useGetSingleNewProductQuery(slug, { skip: !slug });
 
-  if (isLoading)   return <ProductDetailsLoader loading />;
-  if (isError)     return <ErrorMsg msg="There was an error" />;
+  if (isLoading) return <ProductDetailsLoader loading />;
+  if (isError) return <ErrorMsg msg="There was an error" />;
   if (!data?.data) return <ErrorMsg msg="No product found!" />;
 
   const product = mapBackendProductToFrontend(data.data);
