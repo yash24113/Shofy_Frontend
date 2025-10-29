f14ee2bbc7f7eb3350ec08d5fd7e99c42fa9094c
Yash Khalas <yashkharva506@gmail.com>
2025-10-29 10:52:25 +0530

    fix blog error

--- src/app/blog-details/[id]/page.jsx
+++ src/app/blog-details/[id]/page.jsx
@@ -1,19 +1,37 @@
-import React from "react";
+// app/blog-details/[id]/page.jsx
 import HeaderTwo from "@/layout/headers/header-2";
 import Wrapper from "@/layout/wrapper";
 import Footer from "@/layout/footers/footer";
 import BlogDetailsArea from "@/components/blog-details/blog-details-area";
-import blogData from "@/data/blog-data";
 
-const BlogDetails = ({ params }) => {
-  const blogItem = blogData.find((b) => Number(b.id) === Number(params.id));
+const API_BASE = (process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000").replace(/\/+$/,'');
+const BLOG_PATH = process.env.NEXT_PUBLIC_API_BLOG_PATH || "/blogs";
+
+async function getBlog(id) {
+  const res = await fetch(`${API_BASE}${BLOG_PATH}/${id}`, { cache: "no-store" });
+  if (!res.ok) return null;
+  const json = await res.json();
+  return json?.data ?? null;
+}
+
+export async function generateMetadata({ params }) {
+  const blog = await getBlog(params.id);
+  return {
+    title: blog?.title ? `${blog.title} | Blog` : "Blog Details",
+    description: blog?.heading || blog?.paragraph1 || "",
+    openGraph: { images: [blog?.blogimage1 || blog?.blogimage2].filter(Boolean) },
+  };
+}
+
+export default async function BlogDetails({ params }) {
+  const blog = await getBlog(params.id);
+
   return (
     <Wrapper>
-      <HeaderTwo style_2={true} />
-      <BlogDetailsArea blog={blogItem} />
-      <Footer primary_style={true} />
+      <HeaderTwo style_2 />
+      {/* You can show a nicer 404 section if blog is null */}
+      <BlogDetailsArea blog={blog} />
+      <Footer primary_style />
     </Wrapper>
   );
-};
-
-export default BlogDetails;
+}
